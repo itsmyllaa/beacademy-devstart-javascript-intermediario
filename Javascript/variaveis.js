4ef0f6fc64d32df187a30bddeded40c322cc4709
Camila <itsmyllaa@github.com>
2023-03-05 23:26:21 -0300

    Aprendendo a usar as variáveis na prática

---
+++ Javascript/variaveis.js
@@ -0,0 +1,12 @@
+/*Template Strings*/
+
+let firstName = "José";
+let lastName = "da Silva";
+
+let text = `Welcome ${firstName} ${lastName}`;
+//console.log(text)
+
+let price = 20;
+let fees = 0.50;
+let total = `O total do produto é: R$ ${(price + fees)}`;
+console.lo(total)
